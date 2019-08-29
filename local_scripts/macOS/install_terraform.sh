7636dc364c93c6aaefd66d00c9784b25bfd59da1
zalkar-z <zalkar@bennington.edu>
2019-08-29 13:16:57 -0400

    homebrew silent install complete

--- local_scripts/macOS/install_terraform.sh
+++ local_scripts/macOS/install_terraform.sh
@@ -1,4 +1,11 @@
-# Bash script to automate Terraform installation on Linux/Unix machines
+# Bash script to automate Terraform installation on macOS
 
 # 1 - Install homebrew
+URL_BREW='https://raw.githubusercontent.com/Homebrew/install/master/install'
+
+echo -n '- Installing brew ... '
+echo | /usr/bin/ruby -e "$(curl -fsSL $URL_BREW)" > /dev/null
+if [ $? -eq 0 ]; then echo 'Homebrew installed successfully.'; else echo 'Installation failed.'; fi
+
+
 # 2 - Install Terraform
