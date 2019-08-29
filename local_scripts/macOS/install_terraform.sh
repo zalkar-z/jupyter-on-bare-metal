609f242c3fcd987fdfc2e538ca5b5e2594e24916
zalkar-z <zalkar@bennington.edu>
2019-08-29 16:38:00 -0400

    conditional install for homebrew added

--- local_scripts/macOS/install_terraform.sh
+++ local_scripts/macOS/install_terraform.sh
@@ -1,10 +1,12 @@
 # Bash script to automate Terraform installation on macOS
 
 # Install Homebrew
-URL_BREW='https://raw.githubusercontent.com/Homebrew/install/master/install'
-echo -n '- Installing Homebrew ... '
-echo | /usr/bin/ruby -e "$(curl -fsSL $URL_BREW)" > /dev/null
-if [ $? -eq 0 ]; then echo 'Homebrew installed successfully.'; else echo 'Homebrew installation failed.'; fi
+if ! [ -x "$(command -v brew)" ]; then
+    URL_BREW='https://raw.githubusercontent.com/Homebrew/install/master/install'
+    echo -n '- Installing Homebrew ... '
+    echo | /usr/bin/ruby -e "$(curl -fsSL $URL_BREW)" > /dev/null
+    if [ $? -eq 0 ]; then echo 'Homebrew installed successfully.'; else echo 'Homebrew installation failed.'; fi
+fi
 
 # Install Terraform
 if ! [ -x "$(command -v terraform)" ]; then
