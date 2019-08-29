51aa3e1f0380bab91be1a772580aed7a9a0eccc3
zalkar-z <zalkar@bennington.edu>
2019-08-29 13:18:34 -0400

    insteractive comments fixed

--- local_scripts/macOS/install_terraform.sh
+++ local_scripts/macOS/install_terraform.sh
@@ -1,11 +1,11 @@
 # Bash script to automate Terraform installation on macOS
 
-# 1 - Install homebrew
+# 1 - Install Homebrew
 URL_BREW='https://raw.githubusercontent.com/Homebrew/install/master/install'
 
-echo -n '- Installing brew ... '
+echo -n '- Installing Homebrew ... '
 echo | /usr/bin/ruby -e "$(curl -fsSL $URL_BREW)" > /dev/null
-if [ $? -eq 0 ]; then echo 'Homebrew installed successfully.'; else echo 'Installation failed.'; fi
+if [ $? -eq 0 ]; then echo 'Homebrew installed successfully.'; else echo 'Homebrew installation failed.'; fi
 
 
 # 2 - Install Terraform
